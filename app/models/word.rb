51ce08bd6bfc1699a25f2994e4b2cd49e51bc0e5
Soma1412 <syama395@mtroyal.ca>
2021-12-23 02:18:39 +0900

    バリデーション設定

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,9 @@
 class Word < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :speech 
+  
+  validates :word_name,     presence: true
+  validates :meaning,       presence: true
+  
+  belongs_to :user
 end
