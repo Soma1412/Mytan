5e0ed889206e13b22a385ada601648070ada6073
Soma1412 <syama395@mtroyal.ca>
2021-12-22 18:33:35 +0900

    カラムを設定

--- db/migrate/20211222082747_create_words.rb
+++ db/migrate/20211222082747_create_words.rb
@@ -1,7 +1,10 @@
 class CreateWords < ActiveRecord::Migration[6.0]
   def change
     create_table :words do |t|
-
+      t.integer        :speech_id
+      t.string         :word_name     ,null:false
+      t.string         :meaning       ,null:false
+      t.text           :example          
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_20_125928) do
+ActiveRecord::Schema.define(version: 2021_12_22_082747) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2021_12_20_125928) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "words", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "speech_id"
+    t.string "word_name", null: false
+    t.string "meaning", null: false
+    t.text "example"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
