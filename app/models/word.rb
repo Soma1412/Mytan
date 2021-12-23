501b4697c259b30dd5b4789bf4f0b9c03cb84eb0
Soma1412 <syama395@mtroyal.ca>
2021-12-23 14:31:30 +0900

    コントローラー記述

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,5 @@
 class Word < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
+  # extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :speech 
 
   validates :word_name,     presence: true
